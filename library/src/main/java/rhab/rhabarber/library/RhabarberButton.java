b8082ba6de35b8a0fb0fd23e3f7cac3f1919d4c4
Julian Horst <julian@rheinfabrik.de>
2015-08-26 11:28:51 +0200

    add rhabarberbutton

---
+++ library/src/main/java/rhab/rhabarber/library/RhabarberButton.java
@@ -0,0 +1,13 @@
+package rhab.rhabarber.library;
+
+
+import android.content.Context;
+import android.widget.Button;
+
+public class RhabarberButton extends Button{
+
+    public RhabarberButton(Context context) {
+        super(context);
+        setText("RHABARBERRIOT");
+    }
+}
